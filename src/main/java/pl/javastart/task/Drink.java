656e9bd5ba1ce648b684aa9f5252686ad0d9e41c
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-01-07 11:12:35 +0100

    Rozwiazanie zadanie 2.3

---
+++ src/main/java/pl/javastart/task/Drink.java
@@ -0,0 +1,12 @@
+package pl.javastart.task;
+
+public class Drink {
+    String name;
+    double price;
+    boolean alcoholUsage;
+    String component1;
+    String component2;
+    String component3;
+
+    int capacity;
+}
