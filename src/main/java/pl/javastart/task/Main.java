580eccad560148da53542e2bef6bc26adb6b1371
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-01-07 11:15:42 +0100

    Rozwiazanie zadanie 2.3 poprawka 1

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,17 +3,17 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-    Drink drink1 = new Drink();
-    drink1.name = "Mojito";
-    drink1.price = 13.0;
-    drink1.alcoholUsage = true;
-    drink1.component1 = "Woda";
-    drink1.component2 = "Sok cytrynowy";
-    drink1.component3 = "Wódka";
-    drink1.capacity = 210;
+        Drink drink1 = new Drink();
+        drink1.name = "Mojito";
+        drink1.price = 13.0;
+        drink1.alcoholUsage = true;
+        drink1.component1 = "Woda";
+        drink1.component2 = "Sok cytrynowy";
+        drink1.component3 = "Wódka";
+        drink1.capacity = 210;
 
         System.out.println("Drink: " +drink1.name + "\n" + "Cena: " + drink1.price + "zł" + "\n" + "Czy zawiera alkohol ? " +
-                drink1.alcoholUsage +"\n" + "Składniki: " + "\n" + drink1.component1 +"\n" +drink1.component2 +"\n" +
+                drink1.alcoholUsage + "\n" + "Składniki: " + "\n" + drink1.component1 + "\n" +drink1.component2 + "\n" +
                 drink1.component3 + "\n" + "Pojemność: " + drink1.capacity + "ml");
     }
 }
